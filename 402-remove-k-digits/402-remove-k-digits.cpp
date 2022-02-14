33bf193dbfe4189d5c52eaa20aef7b974a7aa706
Yogesh Sakle <88970256+y0gesh02@users.noreply.github.com>
2022-02-14 22:27:01 +0530

    Time: 3 ms (91.31%), Space: 6.9 MB (87.58%) - LeetHub

--- 402-remove-k-digits/402-remove-k-digits.cpp
+++ 402-remove-k-digits/402-remove-k-digits.cpp
@@ -4,15 +4,15 @@ public:
         if (num.length() == k) return "0";
         string ans="";
        for (int i = 0; i < num.length(); i++) {
-        while (ans.length() && ans.back() > num[i] && k) {
+        while (!ans.empty() && ans.back() > num[i] && k) {
                ans.pop_back();                                  
                k--;                                   
         }
-           if(ans.length() || num[i]!='0'){
+           if(!ans.empty() || num[i]!='0'){
                ans.push_back(num[i]);
            }
        }
-        while(k>0 && ans.length() ){
+        while(k>0 && !ans.empty() ){
             ans.pop_back();
             k--;
             
