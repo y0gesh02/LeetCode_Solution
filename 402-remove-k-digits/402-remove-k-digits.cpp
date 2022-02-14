65b82e1c00ca6de13f4f334224ab16593426be3f
Yogesh Sakle <88970256+y0gesh02@users.noreply.github.com>
2022-02-14 22:20:31 +0530

    Time: 5 ms (58.37%), Space: 7.1 MB (53.31%) - LeetHub

---
+++ 402-remove-k-digits/402-remove-k-digits.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string removeKdigits(string num, int k) {
+        if (num.length() == k) return "0";
+        string ans="";
+       for (int i = 0; i < num.length(); i++) {
+        while (ans.length() && ans.back() > num[i] && k) {
+               ans.pop_back();                                  
+               k--;                                   
+        }
+           if(ans.length() || num[i]!='0'){
+               ans.push_back(num[i]);
+           }
+       }
+        while(k>0 && ans.length() ){
+            ans.pop_back();
+            k--;
+            
+        }
+        return ans.empty() ? "0" : ans;
+    }
+};
