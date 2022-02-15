2fa7dcf9f0d83ea712c22dc77ceb55d3cf38ec4e
Yogesh Sakle <88970256+y0gesh02@users.noreply.github.com>
2022-02-15 23:10:05 +0530

    Time: 6 ms (11.33%), Space: 6.1 MB (79.75%) - LeetHub

--- 921-minimum-add-to-make-parentheses-valid/921-minimum-add-to-make-parentheses-valid.cpp
+++ 921-minimum-add-to-make-parentheses-valid/921-minimum-add-to-make-parentheses-valid.cpp
@@ -1,22 +1,11 @@
 class Solution {
 public:
     int minAddToMakeValid(string s) {
-        
-        int cnt=0;
-        int miss_match=0;
+        int cnt=0, miss_match=0;
         for(int i=0;i<s.size();i++){
-            if(s[i]=='('){
-               cnt++;
-            }
-            else if( s[i]==')'&& cnt>0){
-               cnt--;
-             }
-            else{
-                //when ) comes ans cnt is zero
-                miss_match++;
-            }
-           
-         
+            if(s[i]=='(')cnt++;
+            else if( s[i]==')'&& cnt>0) cnt--;
+             else  miss_match++;
         }
         return miss_match+cnt;
     }
