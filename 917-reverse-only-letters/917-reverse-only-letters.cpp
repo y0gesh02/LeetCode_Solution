8ac90961fca8f3a3fdea354b3fe2fc09c0f41e22
Yogesh Sakle <88970256+y0gesh02@users.noreply.github.com>
2022-02-17 23:38:04 +0530

    Time: 0 ms (100.00%), Space: 6 MB (88.13%) - LeetHub

---
+++ 917-reverse-only-letters/917-reverse-only-letters.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string reverseOnlyLetters(string s) {
+        int i=0;
+        int j=s.size()-1;
+        while(i<j){
+         if (!isalpha(s[i]))
+                ++i;
+            else if (!isalpha(s[j]))
+                --j;
+            else
+                swap(s[i++], s[j--]);
+        }
+        return s;
+    }
+};
