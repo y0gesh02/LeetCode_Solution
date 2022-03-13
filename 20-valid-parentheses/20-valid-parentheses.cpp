e6e5817ab9492787041c5ed0c063696da2368297
Yogesh Sakle <88970256+y0gesh02@users.noreply.github.com>
2022-03-13 20:12:38 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (10.36%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
     bool isValid(string s) {
-         stack<int>st;
-        int n=s.size();
-
-        for(int i=0;i<n;i++){
-            if(s[i]=='('|| s[i]=='{'|| s[i]=='['){
+        if(s.size()==1)return false;
+        stack<char>st;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='(' || s[i]=='['||s[i]=='{'){
                 st.push(s[i]);
-                continue;
-             }
-           
-           else  if(st.empty() ||  (st.top()=='(' && s[i]!=')') || (st.top()=='{' && s[i]!='}') || (st.top()=='[' && s[i]!=']')){
-               return false;
             }
+            else if((st.empty() || st.top()=='(' &&s[i]!=')'||st.top()=='{' &&s[i]!='}'||st.top()=='[' &&s[i]!=']')){
+                    return false;
+                }
             
-            
-            st.pop();    //if control reaches to that line, it means we have got the right                              //pair of brackets, so just pop it.
+            else{
+               st.pop(); 
             }
+        }
         return st.empty();
     }
 };
