a960ce4df301f8aaf32dd9826e581dc4948fe8cf
Yogesh Sakle <88970256+y0gesh02@users.noreply.github.com>
2022-03-15 19:40:44 +0530

    Time: 16 ms (23.72%), Space: 14.7 MB (98.11%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -1,3 +1,22 @@
+ class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+		if(l1 == NULL)	return l2;
+		if(l2 == NULL)	return l1;
+		// if value pointend by l1 pointer is less than equal to value pointed by l2 pointer
+		// we wall call recursively l1 -> next and whole l2 list.
+		if(l1 -> val < l2 -> val){
+			l1 -> next = mergeTwoLists(l1 -> next, l2);
+			return l1;
+		}
+		// we will call recursive l1 whole list and l2 -> next
+		else{
+			l2 -> next = mergeTwoLists(l1, l2 -> next);
+			return l2;            
+		}
+    }
+};
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -8,31 +27,3 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class Solution {
-public:
-    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        if(l1==NULL)return l2;
-        if(l2==NULL)return l1;
-        ListNode*dummy=new ListNode();
-        ListNode* temp=dummy;
-        while(l1!=NULL && l2!=NULL){
-            if(l1->val < l2->val){
-                temp->next=l1;
-                l1=l1->next;
-                temp=temp->next;
-            }
-            else{
-                 temp->next=l2;
-                l2=l2->next;
-                temp=temp->next;
-            }
-        }
-        if(l1!=NULL){
-            temp->next=l1; 
-        }
-        if(l2!=NULL){
-            temp->next=l2;
-        }
-        return dummy->next;
-    }
-};
