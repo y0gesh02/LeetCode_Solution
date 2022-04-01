863020a65c6980ae953548b8e4fdb6f03f468e81
Yogesh Sakle <88970256+y0gesh02@users.noreply.github.com>
2022-04-01 21:08:20 +0530

    Time: 7 ms (42.31%), Space: 8.9 MB (5.12%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.cpp
+++ 162-find-peak-element/162-find-peak-element.cpp
@@ -18,7 +18,7 @@ public:
            while(start<=end){
             int mid=start+(end-start)/2;
             if(
-               (mid==0 || nums[mid]>=nums[mid-1]) &&
+                (mid==0 || nums[mid]>=nums[mid-1]) &&
                 (mid==n-1 || nums[mid]>=nums[mid+1])
               ){
                 return mid;
@@ -26,12 +26,13 @@ public:
             //if mid zero pe h or mid+1 se bada hai to ye ans hai nums[mid]>=nums[mid+1]
             //if mid  last pe  h or mid-1 se bada hai to ye ans hai nums[mid]>=nums[mid-1]
             else if(mid>0 && nums[mid-1]>=nums[mid]){
-                end=mid-1;
+                                end=mid-1;
+                
             }   
             //aree target ni h to mid ko hi aage piche karna padega 
             //mid >0 to hi move karna warna yad h na runtime error
             else{
-                start=mid+1;
+             start=mid+1;
             }
         }
     return -1;
