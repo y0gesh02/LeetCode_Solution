f0f5310514796a306c2271ab248a9a66b314a12e
Yogesh Sakle <88970256+y0gesh02@users.noreply.github.com>
2022-04-04 00:01:03 +0530

    Time: 4 ms (73.50%), Space: 12.2 MB (37.25%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int numsLen = nums.size();
-        int cnt=-1;
-        for(int i=numsLen-2; i>=0; i--){
-            if(nums[i]<nums[i+1]){
-                cnt=i;
-                break;
+        int n=nums.size();
+        int l,r;
+        for(l=n-2;l>=0;l--)                           // find decreasing sequence
+        {
+            if(nums[l]<nums[l+1]) break;
+        }
+        if(l<0) reverse(nums.begin(),nums.end());
+        else
+        {
+            for(r=n-1;r>l;r--)                       // find rightmost successor to pivot
+            {
+                if(nums[r]>nums[l]) break;
             }
+            swap(nums[l],nums[r]);                  // swap l,r
+            
+            reverse(nums.begin()+l+1,nums.end());   // reverse from l+1 to end
         }
-        if(cnt != -1){
-            for(int i=numsLen-1; i>cnt; i--){
-            if(nums[i]>nums[cnt]){
-                swap(nums[i],nums[cnt]);
-                break;
-            }
-        }
-        }
-        if(cnt != -1) reverse(nums.begin()+cnt+1,nums.end());
-        else reverse(nums.begin(),nums.end());
     }
 };
+//https://leetcode.com/problems/next-permutation/discuss/1909229/Simple-easy-c%2B%2B-solution
