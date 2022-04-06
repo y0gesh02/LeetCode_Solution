f9669883a70fb8917081764ce5fe40df38095c39
Yogesh Sakle <88970256+y0gesh02@users.noreply.github.com>
2022-04-06 23:54:44 +0530

    Time: 19 ms (15.11%), Space: 10.7 MB (9.53%) - LeetHub

--- 414-third-maximum-number/414-third-maximum-number.cpp
+++ 414-third-maximum-number/414-third-maximum-number.cpp
@@ -4,8 +4,8 @@ public:
        int thirdMax(vector<int>& nums) {
         set<int> s;
         
-        for(int &n: nums)
-            s.insert(n);
+        for(auto it: nums)
+            s.insert(it);
         
         return s.size()<3 ? *max_element(s.begin(),s.end()) : *next(s.begin(), s.size() - 3);
     }
