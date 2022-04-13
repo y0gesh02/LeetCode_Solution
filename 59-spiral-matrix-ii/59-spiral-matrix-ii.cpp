4fdc5ff0b7c682240843f6cfb48fbb065569aca2
Yogesh Sakle <88970256+y0gesh02@users.noreply.github.com>
2022-04-13 22:40:14 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (18.13%) - LeetHub

---
+++ 59-spiral-matrix-ii/59-spiral-matrix-ii.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<vector<int>> generateMatrix(int n) {
+         vector<vector<int>>ans (n,vector<int>(n,0));
+        int rowstart=0;
+        int rowend=n-1;
+        int colstart=0;
+        int colend=n-1;
+       
+        int count=1;
+        int total=n*n;
+        while(count<=total){
+            for(int i=colstart; count<=total && i<=colend;i++){
+                ans[rowstart][i]=count;
+                count++;
+            }
+            rowstart++;
+           for(int i=rowstart; count<=total && i<=rowend;i++){
+                ans[i][colend]=count;
+               count++;
+            }
+            colend--;
+             for(int i=colend; count<=total && i>=colstart;i--){
+                ans[rowend][i]=count;
+                 count++;
+            }
+            rowend--;
+             for(int i=rowend;  count<=total && i>=rowstart;i--){
+                ans[i][colstart]=count;
+                 count++;
+            }
+           colstart++;
+        }
+        return ans;
+    }
+};
