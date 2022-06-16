ed06612926260a3905bc58ffed45426820bc2434
Yogesh Sakle <88970256+y0gesh02@users.noreply.github.com>
2022-06-16 19:35:09 +0530

    Time: 1073 ms (5.02%), Space: 156.8 MB (7.10%) - LeetHub

--- 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
@@ -1,31 +1,41 @@
 class Solution {
 public:
     int maximumUniqueSubarray(vector<int>& nums) {
-       unordered_map<int,int>mp;
+       set<int>s;
         int maxi=0;
         int i=0;
         int j=0;
         int sum=0;
         while(j<nums.size()){
             sum+=nums[j];
-            mp[nums[j]]++;
-            if(mp.size()<j-i+1){
-                while(mp.size()<j-i+1){
-                    mp[nums[i]]--;
+            if(s.find(nums[j])!=s.end()){
+                while(s.find(nums[j])!=s.end()){
+                    s.erase(nums[i]);
                     sum-=nums[i];
-                    if(mp[nums[i]]==0) mp.erase(nums[i]);
                     i++;
                 }
+                
+            }
+            s.insert(nums[j]);
+            
+//             mp[nums[j]]++;
+//             if(mp.size()<j-i+1){
+//                 while(mp.size()<j-i+1){
+//                     mp[nums[i]]--;
+//                     sum-=nums[i];
+//                     if(mp[nums[i]]==0) mp.erase(nums[i]);
+//                     i++;
+//                 }
                  
-            }
-            if(mp.size()==j-i+1){
-                //int temp=accumulate(nums.begin()+i, nums.begin()+j, 0);
+//             }
+//             if(mp.size()==j-i+1){
+//                 //int temp=accumulate(nums.begin()+i, nums.begin()+j, 0);
                maxi=max(maxi,sum);
-              
+              j++;
             }
-           j++;  
+             
            
-        }
+        
         return maxi;
     }
 };
