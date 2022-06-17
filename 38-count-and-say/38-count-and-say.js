7fb0221d5d8c326ebcbd6ff6999e516c925bb0f5
Yogesh Sakle <88970256+y0gesh02@users.noreply.github.com>
2022-06-18 00:12:44 +0530

    Time: 75 ms (81.60%), Space: 44.8 MB (35.78%) - LeetHub

--- 38-count-and-say/38-count-and-say.js
+++ 38-count-and-say/38-count-and-say.js
@@ -3,25 +3,25 @@
  * @return {string}
  */
 var countAndSay = function(n) {
-      
-        let str = '1';
-        if(n === 1) return str;
-        while (n >1) {
-            let curr = "";
-            let count = 0;
-            let say = str[0];
-            for (let i = 0; i < str.length; i++) {
-                  if (str[i] === say) {
-                      count += 1;
-                  }
-                  else {
-                      curr += count + say;
-                      count = 1;
-                      say = str[i];
-                  }
-            }
-            str = curr +count+say;
-             n -= 1;
+         let ans = '1';
+         for(let i=0; i<n-1 ; i++){
+             ans=say(ans);
          }
-  return str;
+         return ans;
 };
+var say = function(temp){
+    let count=0;
+    let curr=temp[0];
+    let str="";
+    
+    for (let i=0; i<=temp.length;i++){
+        if(curr==temp[i]){
+            count++;
+        }else{
+            str +=count+curr;
+            count=1;
+            curr=temp[i]
+        }   
+    }
+return str;    
+}
