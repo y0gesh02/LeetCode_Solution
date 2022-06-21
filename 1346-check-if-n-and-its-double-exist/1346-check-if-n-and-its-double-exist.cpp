92f4ffbaa8064498f5a4ec3152ae546c4f931050
Yogesh Sakle <88970256+y0gesh02@users.noreply.github.com>
2022-06-22 01:06:08 +0530

    Time: 11 ms (52.37%), Space: 10.3 MB (45.25%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
@@ -1,8 +1,10 @@
 class Solution {
 public:
     bool checkIfExist(vector<int>& arr) {
-        unordered_map<int,int>mp;
-        mp[arr[0]]++;
+       // sort(arr.begin().arr.end());
+        
+        unordered_set<int>mp;
+        mp.insert(arr[0]);
         for(int i=1;i<arr.size();i++){
            if(arr[i]%2==0 && mp.count(arr[i]/2)){
                return true;
@@ -10,9 +12,11 @@ public:
             if( mp.count(arr[i]*2) ) {
                    return true;
             }
-            mp[arr[i]]++;
+            mp.insert(arr[i]);
            
         }
         return false;
     }
 };
+// 2 3 5 10
+// 1 7 11 14
