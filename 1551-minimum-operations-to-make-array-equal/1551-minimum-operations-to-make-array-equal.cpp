1125c14c3b3ebb42fdb96fe497b248a2afe0465e
Yogesh Sakle <88970256+y0gesh02@users.noreply.github.com>
2022-06-23 19:52:27 +0530

    Time: 0 ms (100.00%), Space: 6 MB (33.16%) - LeetHub

--- 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.cpp
+++ 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.cpp
@@ -1,15 +1,6 @@
 class Solution {
 public:
     int minOperations(int n) {
-    vector<int>nums(n);
-        for(int i=0;i<n;i++){
-            nums[i] = (2 * i) + 1;
-        }
-        int ans=0;
-        for(int i=0;i<n/2;i++){
-            int diff=nums[n-i-1]-nums[i];
-            ans+=diff/2;
-        }
-        return ans;
+    return n&1 ? n/2*(n/2+1) : n/2*n/2 ;
     }
 };
