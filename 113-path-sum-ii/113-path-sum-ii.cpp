43f3682839b25221b8876402b8da092fa9f4ec0e
Yogesh Sakle <88970256+y0gesh02@users.noreply.github.com>
2022-06-25 16:55:26 +0530

    Time: 12 ms (77.26%), Space: 19.9 MB (81.91%) - LeetHub

--- 113-path-sum-ii/113-path-sum-ii.cpp
+++ 113-path-sum-ii/113-path-sum-ii.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
-          vector<vector<int> > paths;
-        vector<int> path;
-        findPaths(root, targetSum, path, paths);
-        return paths;
+          vector<vector<int> > ans;
+        vector<int> temp;
+        findPaths(root, targetSum, temp, ans);
+        return ans;
     }
-     void findPaths(TreeNode* node, int sum, vector<int>& path, vector<vector<int> >& paths) {
-        if (!node) return;
-        path.push_back(node -> val);
-        if (!(node -> left) && !(node -> right) && sum == node -> val){
-            paths.push_back(path);
+     void findPaths(TreeNode* node, int target, vector<int>& temp, vector<vector<int> >& ans) {
+        if (node==NULL) return;
+        temp.push_back(node -> val);
+        if (node -> left==NULL && node -> right==NULL && target == node -> val){
+            ans.push_back(temp);
         }
          
-        findPaths(node -> left, sum - node -> val, path, paths);
-        findPaths(node -> right, sum - node -> val, path, paths);
-        path.pop_back();
+        findPaths(node -> left, target - node -> val, temp, ans);
+        findPaths(node -> right, target - node -> val, temp, ans);
+        temp.pop_back();
     }
 };
