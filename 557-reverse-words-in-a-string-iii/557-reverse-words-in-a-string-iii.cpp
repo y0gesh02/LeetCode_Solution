d640f935443bd69ebc4aedf10892c3e9d44a3314
Yogesh Sakle <88970256+y0gesh02@users.noreply.github.com>
2022-06-26 23:51:02 +0530

    Time: 15 ms (79.46%), Space: 9.5 MB (98.72%) - LeetHub

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        int last=0;
-        for(int i=0;i<s.size();i++){
-            if(s[i]==' '){
-                reverse(s.begin()+last,s.begin()+i);
-                last=i+1;
-            }
-            if(i==s.size()-1){
-                reverse(s.begin()+last,s.end());
+        int n=s.size();
+        int j=0;
+       // string t="";
+        for(int i=0;i<n;i++){
+          if(s[i]==' '){
+              reverse(s.begin()+j,s.begin()+i);
+              j=i+1;
+          }
+            if(i==n-1){
+                reverse(s.begin()+j,s.end());
             }
         }
         return s;
     }
 };
+//Let's take LeetCode contest
