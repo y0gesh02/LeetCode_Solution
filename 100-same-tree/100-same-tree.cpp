e7a7ba430f8fe7f878a6eb1fc538fe17c3c4c7f6
Yogesh Sakle <88970256+y0gesh02@users.noreply.github.com>
2022-06-28 22:04:13 +0530

    Time: 3 ms (66.23%), Space: 10.2 MB (6.75%) - LeetHub

--- 100-same-tree/100-same-tree.cpp
+++ 100-same-tree/100-same-tree.cpp
@@ -1,21 +1,30 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        if(p==NULL && q==NULL)    return true;
-        if(p==NULL || q==NULL)    return false;
-        return p->val==q->val && isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
-
-            
+    // if(!root) return true;
+        queue<TreeNode*> qy;
+        qy.push(p);
+        qy.push(q);
+        while(!qy.empty()){
+            TreeNode *left=qy.front();
+            qy.pop();
+            TreeNode *right=qy.front();
+            qy.pop();
+            if(left==NULL && right==NULL){
+                continue;
+            }
+            if(left==NULL || right==NULL){
+                return false;
+            }
+            if(left->val!=right->val){
+                return false;
+            }
+            qy.push(left->left);
+            qy.push(right->left);
+            qy.push(left->right);
+            qy.push(right->right);
+        }
+        return true;     
     }
 };
