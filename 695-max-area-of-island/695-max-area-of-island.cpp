c127b8c4344bc34de399f92eb246deae2a07c178
Yogesh Sakle <88970256+y0gesh02@users.noreply.github.com>
2022-06-30 17:01:32 +0530

    Time: 19 ms (90.10%), Space: 23 MB (98.41%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -3,20 +3,11 @@ public:
     int dfs(vector<vector<int>>& grid,int i,int j){
          int n=grid.size();
          int m=grid[0].size();
-         //int cnt=1;
-      
          if(i<0 || j<0 ||i>=n || j>=m || grid[i][j]==0){
-            
-             return 0;
+            return 0;
          }
-       grid[i][j] = 0;
-      //  int l= +1;
-      //  int r =+1;
-      //   int s=dfs(grid,i,j-1)+1;
-      //   int d=dfs(grid,i,j+1)+1;
-      //       cnt+=l+r+s+d;
-      // //  if(grid)
-        return 1+dfs(grid,i-1,j)+dfs(grid,i+1,j)+dfs(grid,i,j-1)+dfs(grid,i,j+1);
+         grid[i][j] = 0;
+         return 1+dfs(grid,i-1,j)+dfs(grid,i+1,j)+dfs(grid,i,j-1)+dfs(grid,i,j+1);
     }
     int maxAreaOfIsland(vector<vector<int>>& grid) {
          int n=grid.size();
@@ -27,10 +18,8 @@ public:
                 if(grid[i][j]==1){
                      maxi=max(maxi,dfs(grid,i,j));
                 }
-                
             }
         }
-       
         return maxi;
     }
 };
