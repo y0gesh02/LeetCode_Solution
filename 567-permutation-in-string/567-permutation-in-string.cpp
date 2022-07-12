5fb5d96a09f8cb219d7ff86f893f71ec5e035f04
Yogesh Sakle <88970256+y0gesh02@users.noreply.github.com>
2022-07-12 21:23:00 +0530

    Time: 10 ms (72.20%), Space: 7.3 MB (75.35%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.cpp
+++ 567-permutation-in-string/567-permutation-in-string.cpp
@@ -1,34 +1,25 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-       
-         vector<int> freq(26,0);
+        vector<int> freq(26,0);
         vector<int> window(26,0);
-        int i=0, j=0;
-        int len=s2.size();
-        int k=s1.size();
-        if(len < k)
-        {
-          return false;
+        int len=s2.size(), k=s1.size();
+        if(len < k) return false;
+        for(int i=0;i<k;i++){
+            window[s1[i]-'a']++;
+            freq[s2[i]-'a']++;
         }
-        while(j<k){
-            freq[s2[j]-'a']+=1;
-            window[s1[j]-'a']+=1;
-            j++;
-        }
-        j-=1;
-        while(j<len){
+        int i=0;
+        while(k<len){
             if(window==freq){
                return true;
             }
-             j+=1;
-            if(j!=len){
-                freq[s2[j]-'a']+=1;
-            }
-            freq[s2[i]-'a']-=1;
-           
+            freq[s2[k]-'a']++;
+            freq[s2[i]-'a']--;
+            k+=1;
             i+=1;
         }
+        if(window==freq)return true;
         return false; 
     }
 };
