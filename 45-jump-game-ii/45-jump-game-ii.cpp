243dd9c7394352b8616d5820483d586f56d0ae1c
Yogesh Sakle <88970256+y0gesh02@users.noreply.github.com>
2022-07-15 23:49:26 +0530

    Time: 30 ms (58.88%), Space: 16.6 MB (52.98%) - LeetHub

---
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        if(nums.size()<2) return 0;   //base case
+        int currjump=nums[0];
+        int n=nums.size();
+        int i=0;
+        int maxjumps=nums[0];
+        int jumps=1;
+        while(i<n-1){
+            maxjumps=max(maxjumps,nums[i]+i);
+            if(currjump==i){
+                currjump=maxjumps;
+                jumps++;
+            }
+            i++;
+        }
+        return jumps;
+    }
+};
