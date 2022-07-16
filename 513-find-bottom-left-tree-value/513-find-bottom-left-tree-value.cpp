2ab7d9cef47132b606bfc0df0130095b09614263
Yogesh Sakle <88970256+y0gesh02@users.noreply.github.com>
2022-07-17 00:23:28 +0530

    Time: 12 ms (86.93%), Space: 21.8 MB (35.56%) - LeetHub

--- 513-find-bottom-left-tree-value/513-find-bottom-left-tree-value.cpp
+++ 513-find-bottom-left-tree-value/513-find-bottom-left-tree-value.cpp
@@ -1,52 +1,22 @@
 class Solution {
 public:
     int findBottomLeftValue(TreeNode* root) {
-		 // if(!root) return 0;
-		 // if(root->left==NULL && root->right==NULL) return root->val;
-		 // int val;
-		 // queue<TreeNode *> q;
-		 // q.push(root);
-		 // while(!q.empty()){
-		 // int n = q.size();
-		 // for(int i=0;i<n;i++){
-		 // TreeNode *node = q.front(); q.pop();
-		 // if(node->left) q.push(node->left);
-		 // if(node->right) q.push(node->right);
-		 // if(node->left and (!node->left->left and !node->left->right))
-		 // val=node->left->val;
-		 // return val;
-		 // if(!node->left && node->right ){
-		 // val=node->right->val;
-		 // }
-		 // }
-		 // }
-		 // return val;
-        
-        queue<TreeNode *> q;
-q.push(root);
-int a;
-
-    while(q.size())
-    {
-        int cnt = 0;
-        int n = q.size();
-        while(n--)
-        {
-            TreeNode *temp = q.front();
-            q.pop();
-            if(cnt == 0)
-                a = temp->val;
-            if(temp->left)
-                q.push(temp->left);
-            if(temp->right)
-                q.push(temp->right);
-            cnt++;
-        }
+		 if(!root) return 0;
+		 int val;
+		 queue<TreeNode *> q;
+		 q.push(root);
+		 while(!q.empty()){
+		    int n = q.size();
+            int cnt=0;
+		    for(int i=0;i<n;i++){
+		        TreeNode *node = q.front(); q.pop();
+                if(cnt==0) val=node->val;
+		        if(node->left) q.push(node->left);
+		        if(node->right) q.push(node->right);
+                cnt++;
+		    }
+		 }
+		 return val;
     }
-    return a;
-    }
-    
 };
-//   3
-//  1  5   
-// 0 2 4 6 
+       
