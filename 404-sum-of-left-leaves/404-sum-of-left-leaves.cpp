4db4b98ee8b1995c7d2d1b53fc83a0a6ac18c07b
Yogesh Sakle <88970256+y0gesh02@users.noreply.github.com>
2022-07-16 23:50:14 +0530

    Time: 7 ms (51.12%), Space: 13.6 MB (10.29%) - LeetHub

--- 404-sum-of-left-leaves/404-sum-of-left-leaves.cpp
+++ 404-sum-of-left-leaves/404-sum-of-left-leaves.cpp
@@ -2,10 +2,20 @@ class Solution {
 public:
     int sum=0;
     int sumOfLeftLeaves(TreeNode* root) {
-        if(!root) return 0;
-        if(root -> left && !root -> left->left&& !root->left->right) sum+=root->left->val;
-        sumOfLeftLeaves(root->left);
-        sumOfLeftLeaves(root->right);
+         if(!root) return 0;
+         int sum = 0;
+		 queue<TreeNode *> q;
+         q.push(root);
+        while(!q.empty()){
+            int n = q.size();
+            for(int i=0;i<n;i++){
+                TreeNode *node = q.front(); q.pop();
+                if(node->left) q.push(node->left);
+                if(node->right) q.push(node->right);
+                if(node->left and (!node->left->left and !node->left->right))
+                    sum += node->left->val;
+            }
+        }
         return sum;
     }
 };
