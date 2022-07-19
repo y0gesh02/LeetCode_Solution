71edaf86966b777f9e1f5d577d91363fc3905be8
Yogesh Sakle <88970256+y0gesh02@users.noreply.github.com>
2022-07-19 23:38:47 +0530

    Time: 344 ms (77.22%), Space: 150.9 MB (53.41%) - LeetHub

--- 1609-even-odd-tree/1609-even-odd-tree.cpp
+++ 1609-even-odd-tree/1609-even-odd-tree.cpp
@@ -6,24 +6,26 @@ public:
         q.push(root);
         int level=0;
         while(!q.empty()){  
-            vector<int>lval;
+            int prev=level%2==0?0:INT_MAX;
             int n=q.size();
             for(int i=0;i<n;i++){
                 TreeNode* t=q.front();q.pop();
-                lval.push_back(t->val);
+                if(level%2==0 && t->val%2==0 || level%2==0 && t->val<=prev )return false;
+                if(level%2!=0 && t->val%2!=0 || level%2!=0 && t->val>=prev )return false;
                 if(t->left) q.push(t->left);
                 if(t->right) q.push(t->right);
+                prev=t->val;
             }
-            for(int i=0;i<n;i++){
-                if(level%2==0){
-                    if(i<n-1 && lval[i]>=lval[i+1])return false;
-                    if(lval[i]%2==0 ) return false;
-                }
-                else{
-                   if(i<n-1 && lval[i]<=lval[i+1])return false;
-                   if( lval[i]%2!=0) return false; 
-                }
-            }
+            // for(int i=0;i<n;i++){
+            //     if(level%2==0){
+            //         if(i<n-1 && lval[i]>=lval[i+1])return false;
+            //         if(lval[i]%2==0 ) return false;
+            //     }
+            //     else{
+            //        if(i<n-1 && lval[i]<=lval[i+1])return false;
+            //        if( lval[i]%2!=0) return false; 
+            //     }
+            // }
             level+=1;
         }
         return true;
