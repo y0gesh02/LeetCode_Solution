87982735bf350521fe83c5a731dc5bbc61de4956
Yogesh Sakle <88970256+y0gesh02@users.noreply.github.com>
2022-08-11 23:18:31 +0530

    Time: 8 ms (61.05%), Space: 20.9 MB (18.78%) - LeetHub

--- 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
@@ -1,42 +1,20 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        vector<string> words;
-        stringstream ss(s);
-        string tmp;
-        while (ss >> tmp)
-            words.push_back(tmp);
-        // words== "the" "sky" "is" "blue"
-       // for(int i=0;i<words.size();i++) cout<<words[i]<<endl;
-        string ans;
-        for (int i = words.size() - 1; i >= 0; --i) {
-            if (i != words.size() - 1) ans += " ";
-            ans += words[i];
-        }
-        return ans;
-//         string temp="";
-//         string ans="";
-//         int n=s.size();
-//         int i=n-1;
-//        while(i>=0){
-//            while(i>=0 && s[i] == ' ') {
-//                i--;
-//            }
-//             if(s[i]!=' ')temp+=s[i];
-             
-//             else  {
-//                 reverse(temp.begin(),temp.end());
-//                 ans+=temp;
-//                 temp="";
-//                 if(i!=0) ans+=' ';
-//             }
-//            i--;
-//         }
-       
-//                 reverse(temp.begin(),temp.end());
-//                 ans+=temp;
-//                 temp="";
-            
-//       return ans;
+        string ans="";
+        int n=s.size();
+        int i=0,j=0;
+        while(i<n){
+           while(i<n && s[i] == ' ') i++;
+           if(i>=n) break;
+           j=i+1;
+           while(j<n && s[j]!=' ') j++;
+           // substr(begin, lenght);
+           string temp=s.substr(i,j-i);
+           if(ans.size()==0) ans=temp;
+           else ans=temp+" "+ans;
+            i=j+1;
+    }
+       return ans;
     }
 };
