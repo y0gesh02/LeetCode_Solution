c8455fb76d099a72327d0db5b7118fc9caa95815
Yogesh Sakle <88970256+y0gesh02@users.noreply.github.com>
2022-08-14 16:25:46 +0530

    Time: 3 ms (70.00%), Space: 5.9 MB (90.00%) - LeetHub

--- 2375-construct-smallest-number-from-di-string/2375-construct-smallest-number-from-di-string.cpp
+++ 2375-construct-smallest-number-from-di-string/2375-construct-smallest-number-from-di-string.cpp
@@ -18,3 +18,10 @@ public:
         return ans;
     }
 };
+
+// 1 2 3 4 5 6 7 8 9
+// D D I D D I D D
+
+// when i==n || pattern[i]=='I'
+// Reverse all numbers between "I".
+//     3 2 1 6 5 4 9 8 7
