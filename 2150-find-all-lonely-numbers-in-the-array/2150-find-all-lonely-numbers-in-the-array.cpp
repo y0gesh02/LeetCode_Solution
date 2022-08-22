dff877d5aac8446c7d76db2b9c92f9cc84c575d6
Yogesh Sakle <88970256+y0gesh02@users.noreply.github.com>
2022-08-22 18:46:29 +0530

    Time: 1506 ms (8.43%), Space: 229.2 MB (13.10%) - LeetHub

--- 2150-find-all-lonely-numbers-in-the-array/2150-find-all-lonely-numbers-in-the-array.cpp
+++ 2150-find-all-lonely-numbers-in-the-array/2150-find-all-lonely-numbers-in-the-array.cpp
@@ -6,7 +6,7 @@ public:
         unordered_map<int ,int>mp;
         for(auto it:nums)mp[it]++;
         for(int i=0;i<n;i++){
-            if(!mp[nums[i]+1] && !mp[nums[i]-1] && mp[nums[i]]==1)ans.push_back(nums[i]);
+            if(mp[nums[i]+1]==0 && mp[nums[i]-1]==0 && mp[nums[i]]==1)ans.push_back(nums[i]);
         }
         return ans;
     }
