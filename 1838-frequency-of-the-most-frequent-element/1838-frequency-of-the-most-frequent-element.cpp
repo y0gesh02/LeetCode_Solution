2c5ae26c3d00003e1acd49b08ff202dba30f788a
Yogesh Sakle <88970256+y0gesh02@users.noreply.github.com>
2022-08-22 23:38:08 +0530

    Time: 386 ms (43.89%), Space: 99.1 MB (19.40%) - LeetHub

--- 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
@@ -17,3 +17,5 @@ public:
         return j-i;
     }
 };
+// Why is (j - i + 1) * A[j]  - sum <= k valid?
+// (j - i + 1) is the length of the window [i, j]. We want to increase all the numbers in the window to equal A[j], the number of operations needed is (j - i + 1) * A[j] - sum which should be <= k. For example, assume the window is [1,2,3], increasing all the numbers to 3 will take 3 * 3 - (1 + 2 + 3) operations.
