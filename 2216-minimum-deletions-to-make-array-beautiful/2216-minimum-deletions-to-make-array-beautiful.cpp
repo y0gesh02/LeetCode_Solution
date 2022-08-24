57561ee0624951f1fd1d0ed2d8430bb2814d1a56
Yogesh Sakle <88970256+y0gesh02@users.noreply.github.com>
2022-08-24 15:23:28 +0530

    Time: 284 ms (55.84%), Space: 121.1 MB (66.86%) - LeetHub

---
+++ 2216-minimum-deletions-to-make-array-beautiful/2216-minimum-deletions-to-make-array-beautiful.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minDeletion(vector<int>& nums) {
+            int shift = 0;
+            for (int i = 0; i < nums.size() - 1; i++)
+            {
+                // after one deletion all ele shift by one
+                if ((i + shift) % 2 == 0)
+                {
+                    if (nums[i] == nums[i + 1])
+                    {
+                        shift++;
+                    }
+                }
+            }
+            if ((nums.size() - shift) % 2 )
+            {
+                shift++;
+            }
+            return shift;
+    }
+};
