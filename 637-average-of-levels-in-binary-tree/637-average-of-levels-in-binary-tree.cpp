b90ae7adac9eb62061fba64cab7d97257bf4fcbe
Yogesh Sakle <88970256+y0gesh02@users.noreply.github.com>
2022-09-02 21:27:27 +0530

    Time: 19 ms (79.48%), Space: 22.5 MB (72.83%) - LeetHub

--- 637-average-of-levels-in-binary-tree/637-average-of-levels-in-binary-tree.cpp
+++ 637-average-of-levels-in-binary-tree/637-average-of-levels-in-binary-tree.cpp
@@ -2,41 +2,19 @@ class Solution {
 public:
     vector<double> averageOfLevels(TreeNode* root) {
         vector<double>ans;
-        if(root==NULL) return ans;
-        queue<TreeNode*>q;
+        queue<TreeNode*> q;
         q.push(root);
-        while(!q.empty()){
+        while(!q.empty()) {
+            double temp=0;
             int n=q.size();
-            double sum=0;
             for(int i=0;i<n;i++){
-                TreeNode* node=q.front();
-                q.pop();
-                sum+=node->val;
-                if(node->left!=NULL) q.push(node->left);
-                if(node->right!=NULL) q.push(node->right);
-               
+                TreeNode* Node=q.front();q.pop();
+                temp+=Node->val;
+                if(Node->left) q.push(Node->left);
+                if(Node->right) q.push(Node->right);
             }
-            ans.push_back(sum/n);
+            ans.push_back(temp/n);
         }
         return ans;
     }
 };
-
-
-
-
-
-
-
-// class Solution {
-// public:
-//    void solve(TreeNode* root,vector<double>&ans){
-//        if(root==NULL)return;
-       
-//    }
-//     vector<double> averageOfLevels(TreeNode* root) {
-//         vector<double>ans;
-//         solve(root,ans);
-//         return ans;
-//     }
-// };
