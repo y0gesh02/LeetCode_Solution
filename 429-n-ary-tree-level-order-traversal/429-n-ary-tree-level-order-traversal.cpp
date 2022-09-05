8f2690de9392f535359cc1b6bdc0c4d2b2720540
Yogesh Sakle <88970256+y0gesh02@users.noreply.github.com>
2022-09-05 18:15:55 +0530

    Time: 21 ms (95.32%), Space: 11.8 MB (34.37%) - LeetHub

--- 429-n-ary-tree-level-order-traversal/429-n-ary-tree-level-order-traversal.cpp
+++ 429-n-ary-tree-level-order-traversal/429-n-ary-tree-level-order-traversal.cpp
@@ -7,17 +7,16 @@ public:
         q.push(root);
         while(!q.empty()){
             int n=q.size();
-            Node* temp=nullptr;
             vector<int>level;
             for(int i=0;i<n;i++){
-                 temp=q.front();
-                 q.pop();
+                Node* temp=q.front();q.pop();
                 for(int i=0;i<temp->children.size();i++){
                     q.push(temp->children[i]);
                 }
                 level.push_back(temp->val);
             }
             ans.push_back(level);
+            
         }
         return ans;
     }
