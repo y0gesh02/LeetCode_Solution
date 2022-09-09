cdc07f5f83413ea80338014cbcbd0a9e11ba1b5c
Yogesh Sakle <88970256+y0gesh02@users.noreply.github.com>
2022-09-09 17:41:26 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (87.42%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
-    void subset(int index,vector<int>&nums,vector<vector<int>>&ans,vector<int>&temp){
-        ans.push_back(temp);// push the current subset into the resultant array
-        for(int i=index;i<nums.size();i++){
-            temp.push_back(nums[i]);// add the current element to consider the subsets corresponding to it
-              subset(i+1,nums,ans,temp);
-              temp.pop_back(); // as this has been used, pop it
+    void solve(int idx,vector<int>&nums,vector<vector<int>>&ans,vector<int>&subset){
+        ans.push_back(subset);
+        for(int i=idx;i<nums.size();i++){
+            subset.push_back(nums[i]);
+            solve(i+1,nums,ans,subset);
+            subset.pop_back();
         }
+        return;
     }
     vector<vector<int>> subsets(vector<int>& nums) {
-         vector<vector<int>>ans;
-         vector<int>temp;
-         subset(0,nums,ans,temp);
+        vector<vector<int>>ans;
+         vector<int>subset;
+         solve(0,nums,ans,subset);
          return ans;
-     }
-
+    }
 };
