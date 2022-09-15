f7a5cf0e895a0a9aebfca14bd605f0c172d96fe8
Yogesh Sakle <88970256+y0gesh02@users.noreply.github.com>
2022-09-15 23:09:42 +0530

    Time: 148 ms (32.20%), Space: 48.5 MB (44.55%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int minijumps=0;
-        for(int i=nums.size()-2;i>=0;i--){
-            minijumps++;
-            if(nums[i]>=minijumps) minijumps=0;
+        int n=nums.size();
+        int mini=0;
+        for(int i=n-2;i>=0;i--){
+            mini++;
+            if(nums[i]>=mini)mini=0;
         }
-        return minijumps==0;
+        return mini==0;
     }
 };
