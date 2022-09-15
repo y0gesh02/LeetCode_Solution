78f5ee51e1975447da2b0e4ec754440bf7b248da
Yogesh Sakle <88970256+y0gesh02@users.noreply.github.com>
2022-09-15 23:23:23 +0530

    Time: 1 ms (43.04%), Space: 5.9 MB (83.39%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -5,14 +5,15 @@ public:
          if(n <= 0) return 0;
          if(n == 1) return 1;
          if(n == 2) return 2;
-        vector<int>dp(n+1,0);
-        dp[1]=1;
-        dp[2]=2;
+        int prev1=2;
+        int prev2=1;
         for(int i=3;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
+            int curri=prev1+prev2;
+            prev2=prev1;
+            prev1=curri;
         }
         
       
-        return dp[n];
+        return prev1;
     }
 };
