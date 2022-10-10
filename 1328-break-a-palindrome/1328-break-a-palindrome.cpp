c4daa744f5bd686dfae0a90e07d55dcb2774531d
Yogesh Sakle <88970256+y0gesh02@users.noreply.github.com>
2022-10-10 22:42:43 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (78.51%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.cpp
+++ 1328-break-a-palindrome/1328-break-a-palindrome.cpp
@@ -3,15 +3,13 @@ public:
     string breakPalindrome(string palindrome) {
         int n=palindrome.size();
         if(n==1)return "";
-        bool f=false;
         for(int i=0;i<n/2;i++){
             if(palindrome[i]!='a'){
                 palindrome[i]='a';
-                f=true;
-                break;
+               return palindrome;
           }
         }
-        if(!f)palindrome[n-1]='b';
+        palindrome[n-1]='b';
         return palindrome;
     }
 };
