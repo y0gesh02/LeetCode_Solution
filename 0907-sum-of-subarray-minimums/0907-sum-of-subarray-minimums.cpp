e4252165f2a2586f318b9b439c6da3871f0bdd7b
Yogesh Sakle <88970256+y0gesh02@users.noreply.github.com>
2022-11-03 16:12:26 +0530

    Time: 104 ms (94.10%), Space: 43 MB (45.07%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
@@ -5,18 +5,18 @@ public:
         int n = arr.size();
         vector<int> left(n,-1), right(n,n);
         stack<int> st;
-        for(int i=0; i<n; i++){
-            while(st.size() && arr[i] < arr[st.top()]) st.pop();
-            if(st.size()) left[i] = i - st.top();
-            else left[i] = i+1;
-            st.push(i);
-        }
-        while(st.size()) st.pop();
-        for(int i=n-1; i>=0; i--){
-            while(st.size() && arr[i] <= arr[st.top()]) st.pop();
-            if(st.size()) right[i] = st.top() - i;
-            else right[i] = n - i;
-            st.push(i);
+         for(int i=0; i<n; i++){  //left NSL
+             while(!st.empty() && arr[st.top()]>arr[i])st.pop();  //dealing with idx
+             if(st.empty())left[i]=i+1; // assign curr idx
+             else left[i]=i-st.top(); //
+             st.push(i);
+         } 
+         while(!st.empty()) st.pop();
+         for(int i=n-1; i>=0; i--){
+            while(!st.empty() &&  arr[st.top()]>=arr[i]) st.pop();
+            if(st.empty()) right[i] = n - i;
+            else  right[i] = st.top() - i;
+              st.push(i);
         }
         int res = 0;
         for(int i=0; i<n; i++) {
@@ -26,5 +26,26 @@ public:
         }
         
         return res%MOD;
+        
     }
 };
+
+
+
+
+
+ 
+//         for(int i=0; i<n; i++){
+//             while(st.size() && arr[i] < arr[st.top()]) st.pop();
+//             if(st.size()) left[i] = i - st.top();
+//             else left[i] = i+1;
+//             st.push(i);
+//         }
+        
+//         for(int i=n-1; i>=0; i--){
+//             while(st.size() && arr[i] <= arr[st.top()]) st.pop();
+//             if(st.size()) right[i] = st.top() - i;
+//             else right[i] = n - i;
+//             st.push(i);
+//         }
+        
