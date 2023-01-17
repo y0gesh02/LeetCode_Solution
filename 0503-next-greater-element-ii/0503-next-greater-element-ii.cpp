6e0cad9e88d5e98aef59935c8224af8f46fc422e
Yogesh Sakle <88970256+y0gesh02@users.noreply.github.com>
2023-01-17 20:30:54 +0530

    Time: 36 ms (85.29%), Space: 25.1 MB (18.62%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -1,35 +1,26 @@
 class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& arr) {
+        //as circular array first put arr ele in stack
         stack<int> st;
-		vector<int> ans;
-		for(int i=arr.size()-1;i>=0;i--){
-			st.push(arr[i]);
+        vector<int> ans;
+        int n=arr.size();
+        for(int i=n-1;i>=0;i--){
+			st.push(arr[i]); //121
         }
-        
-        int i=arr.size()-1;
-        while(i>=0){
+        for(int i=n-1;i>=0;i--){
             while(!st.empty() && st.top()<=arr[i]){
                 st.pop();
             }
-           if(st.empty()){
-				ans.push_back(-1);
-			}
-			else{
-				ans.push_back(st.top());
-			}
+            if(st.empty()){
+                ans.push_back(-1);
+            }
+            else{
+                ans.push_back(st.top());
+            }
             st.push(arr[i]);
-            i--;
         }
-       reverse(ans.begin(),ans.end());
-
+        reverse(ans.begin(),ans.end());
         return ans;
     }
 };
-//1 2 1 1 2 1
-// 2 -1 2 2-1 -1         -1
-// 2 -1 -1
-// -1 -1  2
-    
-// 2 3    4 -1 -1
-// -1 -1 -1 -1 4
